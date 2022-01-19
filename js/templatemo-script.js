c0b141674b09ac7a6bc0322538f55fc836fc9f73
RONUN314 <62382242+RONUN314@users.noreply.github.com>
2022-01-19 09:36:19 +0700

    Update templatemo-script.js

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,10 +1,4 @@
-/*
-
-TemplateMo 560 Astro Motion
-
-https://templatemo.com/tm-560-astro-motion
-
-*/
+ 
 
 var gallery = undefined;
 
@@ -47,9 +41,7 @@ function setupGallery() {
           slidesToScroll: 2
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+   
     ]
   });
 }
